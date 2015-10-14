df8a99b220858ae749b75fd25137c9c1cd638ffb
Steven Schulte <shotgunsteven4@yahoo.com>
2015-10-14 17:30:30 -0600

    Fake data completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create Users
+5.times do
+   u = User.new(
+    email:    Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+  u.skip_confirmation!
+  u.save
+end
+users = User.all
+
+# Create Wikis
+50.times do
+ # #1
+   Wiki.create!(
+ # #2
+     title:  Faker::Lorem.sentence,
+     body:   Faker::Lorem.paragraph(2, true),
+     private: false,
+     user: users.sample
+   )
+end
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
