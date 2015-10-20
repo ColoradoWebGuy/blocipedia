efce86ff798e8257ba15a2b3624df04f8e785835
Steven Schulte <shotgunsteven4@yahoo.com>
2015-10-20 09:43:16 -0600

    Finalized project

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -4,17 +4,5 @@ class Wiki < ActiveRecord::Base
   has_many :collaborators
   has_many :users, through: :collaborators
 
-
-  scope :visible_to, -> (user) {
-    if user
-      if (user.admin? || user.premium?)
-        all
-      else
-        where(private: false)
-      end
-    else
-      where(private: false)
-    end
-  }
-
+  default_scope { order('created_at DESC') }
 end
